9d4879cde965c1e1ee1d5f42ee9fd8a2777f142b
Strets123 <strets123@gmail.com>
2016-04-17 15:35:24 +0100

    pep8 formatting

--- grapper/grapper.py
+++ grapper/grapper.py
@@ -6,15 +6,16 @@ import json
 from os import linesep
 
 
-STOP_TOKEN="STOP!!!"
+STOP_TOKEN = "STOP!!!"
 
 
 def file_writer(dest_filename, some_queue, some_stop_token):
-    """Write JSON strings to a JSON list from a multiprocessing queue to a file until the stop token is sent"""
+    """Write JSON strings to a JSON list from a multiprocessing
+    queue to a file until the stop token is sent"""
     is_start_of_json = True
     with open(dest_filename, 'w') as dest_file:
         dest_file.write("[")
-        while True: 
+        while True:
             line = some_queue.get()
             if line == some_stop_token:
                 dest_file.write(linesep)
@@ -28,36 +29,36 @@ def file_writer(dest_filename, some_queue, some_stop_token):
             dest_file.write(line)
 
 
-
-
-
-
 def remap_genome_coordinate(coord, align_dict):
-    """Given a dictionary of chromosome alignment remappings, 
+    """Given a dictionary of chromosome alignment remappings,
     remap a single coordinate"""
     original_chromosome = coord["chromosome"]
     chromosome_mapping = align_dict.get(original_chromosome, None)
     if chromosome_mapping is not None:
         source_start_point = chromosome_mapping["source"]["start"]
-        bases_from_start_of_chromosome = coord["position"] - source_start_point
-        if bases_from_start_of_chromosome >= 0 and bases_from_start_of_chromosome <= chromosome_mapping["length"]:
-            #The base from the coordinate is within range
+        bases_from_start = coord["position"] - source_start_point
+        within_range = bases_from_start <= chromosome_mapping["length"]
+        if bases_from_start >= 0 and within_range:
+            # The base from the coordinate is within range
             new_start_point = chromosome_mapping["target"]["start"]
             new_chromosome = chromosome_mapping["target"]["chromosome"]
             coord["chromosome"] = new_chromosome
-            coord["position"] = new_start_point + bases_from_start_of_chromosome
+            coord["position"] = new_start_point + bases_from_start
             return coord
     return None
 
 
-
-
-def remap_reference_genome(alignment_file_path, coordinate_file_path, writer_queue):
-    """Given the file path to an alignment file and the file path to a coordinate file 
-    write an output file which maps the source genome coordinates to a new reference genome"""
+def remap_reference_genome(alignment_file_path,
+                           coordinate_file_path,
+                           writer_queue):
+    """Given the file path to an alignment file and the
+    file path to a coordinate file
+    write an output file which maps
+    the source genome coordinates to a new reference genome"""
     with open(alignment_file_path, 'r') as align:
         alignments = ijson.items(align, 'item')
-        align_dict = {item["source"]["chromosome"] : item for item in alignments}
+        align_dict = {item["source"]["chromosome"]: item
+                      for item in alignments}
         with open(coordinate_file_path, 'r') as coordfile:
             coords = ijson.items(coordfile, 'item')
             for index, coord in enumerate(coords):
@@ -66,16 +67,18 @@ def remap_reference_genome(alignment_file_path, coordinate_file_path, writer_que
                     writer_queue.put(json.dumps(data_dict))
 
 
-    
-
-
-
 def get_writer_process_and_queue(output):
-    """Returns a multiprocessing process to write to a file and a queue to do the writing"""
+    """Returns a multiprocessing process to write to a
+     file and a queue to do the writing"""
     queue = multiprocessing.Queue()
-    return (multiprocessing.Process(target = file_writer, args=(output, queue, STOP_TOKEN)), queue)
-
-
+    return (
+        multiprocessing.Process(
+            target=file_writer,
+            args=(
+                output,
+                queue,
+                STOP_TOKEN)),
+        queue)
 
 
 def handle_command(alignfile, coordsfile, output):
@@ -87,8 +90,6 @@ def handle_command(alignfile, coordsfile, output):
     writer_process.join()
 
 
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("alignfile", help="Path to the alignment JSON file")
@@ -96,7 +97,3 @@ if __name__ == '__main__':
     parser.add_argument("output", help="Path to the desired output file")
     args = parser.parse_args()
     handle_command(args.alignfile, args.coordsfile, args.output)
-
-    
-
-
--- tests/test_grapper.py
+++ tests/test_grapper.py
@@ -9,11 +9,9 @@ Tests for `grapper` module.
 """
 
 import unittest
-
 from grapper import grapper
 import os
 import json
-import multiprocessing
 
 OUTPUT_FILE = "test_output.json"
 
@@ -26,19 +24,18 @@ class TestGrapper(unittest.TestCase):
         except OSError:
             pass
 
-
     def tearDown(self):
         try:
             os.remove(OUTPUT_FILE)
         except OSError:
             pass
 
-
     def test_file_writer(self):
         """Given a writer process and writer writer queue,
         When I send JSON strings to the queue,
         Then a valid JSON list will be written to the output file specified"""
-        writer_process, writer_queue = grapper.get_writer_process_and_queue(OUTPUT_FILE)
+        writer_process, writer_queue = grapper.get_writer_process_and_queue(
+            OUTPUT_FILE)
         writer_process.start()
         writer_queue.put('1')
         writer_queue.put('"1"')
@@ -48,70 +45,66 @@ class TestGrapper(unittest.TestCase):
         with open(OUTPUT_FILE, 'r') as output:
             jsondata = json.load(output)
             dict_list = [coord for coord in jsondata]
-            self.assertEqual(dict_list, [1,"1",{"1": 1 }])
-
-
-
+            self.assertEqual(dict_list, [1, "1", {"1": 1}])
 
     def test_remap_genome_coordinate(self):
         """Given a valid coordinate on the old reference genome,
         And a dictionary of mappings
         When I call remap_genome_coordinates
         Then I gent back the new coordinate"""
-        coordinate = { "chromosome": "1", "position": 150, "reference": "A" }
+        coordinate = {"chromosome": "1", "position": 150, "reference": "A"}
         align_dict = {
-                        "1": 
-                                {   "source": { "start": 100, "chromosome": "1"}, 
-                                    "length": 100, 
-                                    "target": {"start": 300, "chromosome": "2"}
-                                }, 
-                        "2": 
-                            {   "source": {"start": 300, "chromosome": "2"}, 
-                                "length": 200, 
-                                "target": {"start": 20, "chromosome": "7"}
-                            }
-                    }
+            "1":
+            {"source": {"start": 100, "chromosome": "1"},
+             "length": 100,
+             "target": {"start": 300, "chromosome": "2"}
+             },
+            "2":
+            {"source": {"start": 300, "chromosome": "2"},
+             "length": 200,
+             "target": {"start": 20, "chromosome": "7"}
+             }
+        }
         new_mapping = grapper.remap_genome_coordinate(coordinate, align_dict)
-        self.assertEqual(new_mapping, { "chromosome": "2", "position": 350, "reference": "A" })
-
+        self.assertEqual(
+            new_mapping, {
+                "chromosome": "2", "position": 350, "reference": "A"})
 
     def test_remap_position_outside_expected_range(self):
         """Given a position coordinate outside of the allowed bounds for the chromosome
         And a dictionary of mappings
         When I call remap_genome_coordinates
         Then I gent back None"""
-        coordinate = { "chromosome": "1", "position": 35, "reference": "A" }
+        coordinate = {"chromosome": "1", "position": 35, "reference": "A"}
         align_dict = {
-                        "1": 
-                                {   "source": { "start": 100, "chromosome": "1"}, 
-                                    "length": 100, 
-                                    "target": {"start": 300, "chromosome": "2"}
-                                }, 
-                        "2": 
-                            {   "source": {"start": 300, "chromosome": "2"}, 
-                                "length": 200, 
-                                "target": {"start": 20, "chromosome": "7"}
-                            }
-                    }
+            "1":
+            {"source": {"start": 100, "chromosome": "1"},
+             "length": 100,
+             "target": {"start": 300, "chromosome": "2"}
+             },
+            "2":
+            {"source": {"start": 300, "chromosome": "2"},
+             "length": 200,
+             "target": {"start": 20, "chromosome": "7"}
+             }
+        }
         new_mapping = grapper.remap_genome_coordinate(coordinate, align_dict)
         self.assertEqual(new_mapping, None)
-        coordinate = { "chromosome": "1", "position": 201, "reference": "A" }
+        coordinate = {"chromosome": "1", "position": 201, "reference": "A"}
         align_dict = {
-                        "1": 
-                                {   "source": { "start": 100, "chromosome": "1"}, 
-                                    "length": 100, 
-                                    "target": {"start": 300, "chromosome": "2"}
-                                }, 
-                        "2": 
-                            {   "source": {"start": 300, "chromosome": "2"}, 
-                                "length": 200, 
-                                "target": {"start": 20, "chromosome": "7"}
-                            }
-                    }
+            "1":
+            {"source": {"start": 100, "chromosome": "1"},
+             "length": 100,
+             "target": {"start": 300, "chromosome": "2"}
+             },
+            "2":
+            {"source": {"start": 300, "chromosome": "2"},
+             "length": 200,
+             "target": {"start": 20, "chromosome": "7"}
+             }
+        }
         new_mapping = grapper.remap_genome_coordinate(coordinate, align_dict)
         self.assertEqual(new_mapping, None)
-
-
 
     def test_chromosome_not_mapped(self):
         """Given an input coordinate for which the chromosome is not mapped
@@ -119,38 +112,40 @@ class TestGrapper(unittest.TestCase):
         When I call remap_genome_coordinates
         Then I gent back None"""
 
-        coordinate = { "chromosome": "12", "position": 150, "reference": "A" }
+        coordinate = {"chromosome": "12", "position": 150, "reference": "A"}
         align_dict = {
-                        "1": 
-                                {   "source": { "start": 100, "chromosome": "1"}, 
-                                    "length": 100, 
-                                    "target": {"start": 300, "chromosome": "2"}
-                                }, 
-                        "2": 
-                            {   "source": {"start": 300, "chromosome": "2"}, 
-                                "length": 200, 
-                                "target": {"start": 20, "chromosome": "7"}
-                            }
-                    }
+            "1":
+            {"source": {"start": 100, "chromosome": "1"},
+             "length": 100,
+             "target": {"start": 300, "chromosome": "2"}
+             },
+            "2":
+            {"source": {"start": 300, "chromosome": "2"},
+             "length": 200,
+             "target": {"start": 20, "chromosome": "7"}
+             }
+        }
         new_mapping = grapper.remap_genome_coordinate(coordinate, align_dict)
         self.assertEqual(new_mapping, None)
 
-
-
     def test_handle_command(self):
-        """Given the JSON alignment file in tests/test_data/alignment.json with contents
+        """Given the JSON alignment file
+        in tests/test_data/alignment.json with contents
 
-        [{ "length": 100, "source": { "chromosome": "1", "start": 100 }, "target": { "chromosome": "2", "start": 
+        [{ "length": 100,
+        "source": { "chromosome": "1", "start": 100 },
+        "target": { "chromosome": "2", "start": 300 } },
 
-        300 } },
-
-        { "length": 200, "source": { "chromosome": "2", "start": 300 }, "target": { "chromosome": "7", "start": 
+        { "length": 200,
+        "source": { "chromosome": "2", "start": 300 },
+        "target": { "chromosome": "7", "start":
 
         20 } }
 
         ]
 
-        And the JSON source coordinate file in tests/test_data/source_coordinates.json with contents
+        And the JSON source coordinate file in
+        tests/test_data/source_coordinates.json with contents
 
         [{ "chromosome": "1", "position": 150, "reference": "A" },
 
@@ -168,7 +163,7 @@ class TestGrapper(unittest.TestCase):
 
         ]
         """
-        
+
         try:
             os.remove(OUTPUT_FILE)
         except OSError:
@@ -177,32 +172,34 @@ class TestGrapper(unittest.TestCase):
         coordsfile = "tests/test_data/source_coordinates.json"
 
         grapper.handle_command(alignfile, coordsfile, OUTPUT_FILE)
-        #Wait for file to be fully flushed to the disk
+        # Wait for file to be fully flushed to the disk
         with open(OUTPUT_FILE, 'r') as output:
             target_coords = json.load(output)
             dict_list = [coord for coord in target_coords]
-            self.assertEqual(dict_list, [{ "chromosome": "2", "position": 350, "reference": "A" },
-                                            { "chromosome": "7", "position": 20, "reference": "C" }])
+            self.assertEqual(dict_list, [{"chromosome": "2",
+                                          "position": 350,
+                                          "reference": "A"},
+                                         {"chromosome": "7",
+                                          "position": 20,
+                                          "reference": "C"}])
 
         os.remove(OUTPUT_FILE)
 
-
-
-
     def test_handle_outside_range_or_unmapped(self):
-        """Given the JSON alignment file in tests/test_data/alignment.json with contents
+        """Given the JSON alignment file in
+        tests/test_data/alignment.json with contents
 
-        [{ "length": 100, "source": { "chromosome": "1", "start": 100 }, "target": { "chromosome": "2", "start": 
+        [{ "length": 100,
+        "source": { "chromosome": "1", "start": 100 },
+        "target": { "chromosome": "2", "start": 300 } },
 
-        300 } },
-
-        { "length": 200, "source": { "chromosome": "2", "start": 300 }, "target": { "chromosome": "7", "start": 
-
-        20 } }
+        { "length": 200, "source": { "chromosome": "2", "start": 300 },
+        "target": { "chromosome": "7", "start": 20 } }
 
         ]
 
-        And the JSON source coordinate file in tests/test_data/source_coordinates_with_invalid.json with contents
+        And the JSON source coordinate file in
+        tests/test_data/source_coordinates_with_invalid.json with contents
 
         [{ "chromosome": "1", "position": 150, "reference": "A" },
 
@@ -229,21 +226,20 @@ class TestGrapper(unittest.TestCase):
         ]
         """
 
-
-
         alignfile = "tests/test_data/alignment.json"
         coordsfile = "tests/test_data/source_coordinates_with_invalid.json"
 
         grapper.handle_command(alignfile, coordsfile, OUTPUT_FILE)
-        #Wait for file to be fully flushed to the disk
+        # Wait for file to be fully flushed to the disk
         with open(OUTPUT_FILE, 'r') as output:
             target_coords = json.load(output)
             dict_list = [coord for coord in target_coords]
-            self.assertEqual(dict_list, [{ "chromosome": "2", "position": 350, "reference": "A" },
-                                            { "chromosome": "7", "position": 20, "reference": "C" }])
-
-
-
+            self.assertEqual(dict_list, [{"chromosome": "2",
+                                          "position": 350,
+                                          "reference": "A"},
+                                         {"chromosome": "7",
+                                          "position": 20,
+                                          "reference": "C"}])
 
 
 if __name__ == '__main__':
